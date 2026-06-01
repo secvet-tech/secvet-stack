d9abfb6d1f2750a3a9513db07c28f5e3ab08acf9
_kentho <kenneththomsennmbk@gmail.com>
2026-05-31 21:54:30 -0700

    made a manifest, working through the script, debugging

--- script.js
+++ script.js
@@ -2,19 +2,23 @@
 const form = document.getElementById('buddychat');
 form.addEventListener('submit', (e) => {
 e.preventDefault();
+//took out a brace and parenth
 const userinput = document.getElementById('buddybox').value;
 const response = fetch('/api/chat', {
 method: 'POST',
-headers: {'Content-Type': 'application/json', 'Accept': 'application/json'},
+headers: {'Content-Type': 'application/json'}, //'Accept': 'application/json'},
 body: JSON.stringify({userinput})
 });
-//.then(data = response.json())
-//.then(document.getElementById('buddyresponse')).textContent= data.message;
-const data = JSON.stringify({response}); 
+const data = JSON.stringify(response); 
 const buddyresponse = document.getElementById('buddyresponse');
+buddyresponse.addEventListener('change', (event) => {
 buddyresponse.innerText = data;
 console.log('is the transmission successful', data);
 });
+});
 window.addEventListener('load', (event) => {
+console.log(event);
+});
+window.addEventListener('change', (event) => {
 console.log(event);
 });
